4adce4ee37b2dd16d9130ca9e47e6ca211242ef5
Rata Fabian <rata.fabian1@gmail.com>
2024-09-13 22:50:52 +0100

    updating branding page

--- src/components/Branding/Branding.jsx
+++ src/components/Branding/Branding.jsx
@@ -3,6 +3,7 @@ import  businessCardOne  from "../../assets/images/artech_visit_cart_1.webp"
 import  businessCardTwo  from "../../assets/images/artech_visit_card_2.webp"
 import whiteBackground from "../../assets/images/home_white_background.webp"
 import { BsPencilSquare } from "react-icons/bs"
+import { Link } from "react-router-dom"
 
 
 const HomeBranding = () => {
@@ -31,29 +32,44 @@ const HomeBranding = () => {
         </div>
         
         <div className="second_paravam">
-        <h1>ce suntem</h1>
-          <h2>p one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h2>
+        <h1>Branding de top</h1>
+          <h2>Creăm un brand unic și memorabil care reflectă perfect identitatea afacerii tale. Indiferent dacă optezi pentru un stil modern, elegant sau clasic, ne asigurăm că afacerea ta va rămâne în mintea clienților. Fiecare detaliu contează – de la nume și logo, până la paletele de culori, îți oferim design grafic de înaltă calitate care comunică clar și eficient mesajul tău.
+            </h2>
         </div>
 
         <div className="second_parallax">
         <div className="ceata"></div> 
-           <h3 className="branding_titles">LOGOS</h3>
+           <h3 className="branding_titles">LOGOURI</h3>
         </div>
 
         <div className="third_paravan">
-          <h1>ce suntem</h1>
-          <h2>p one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h2>
+          <h1>Design de Logo Personalizat</h1>
+          <h2>Descoperă arta unui logo unic cu Artech. Înțelegem că logo-ul este inima identității vizuale a afacerii tale, de aceea oferim soluții de design personalizate, fie că ești un start-up în creștere sau o companie bine stabilită. Fiecare logo este creat de la zero, special pentru tine, iar colaborarea noastră îndeaproape ne permite să înțelegem viziunea și valorile tale. Oferim multiple concepte și revizii până când ești complet mulțumit de rezultatul final.</h2>
         </div>
 
         <div className="third_parallax">
         <div className="ceata"></div>
-           <h3 className="branding_titles">LOGOS</h3>
+           <h3 className="branding_titles">CARTI DE VIZITA</h3>
         </div>
 
         <div className="second_paravam">
-        <h1>ce suntem</h1>
-          <h2>p one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</h2>
+        <h1>Cărți de Vizită Profesionale</h1>
+          <h2>O carte de vizită este mai mult decât o simplă bucată de hârtie, este o reflectare a identității tale. La Artech, creăm cărți de vizită elegante și profesionale care fac o impresie durabilă. Fiecare design este personalizat și adaptat nevoilor tale specifice, reflectând identitatea și valorile afacerii tale. Oferim o varietate de opțiuni de personalizare, de la dimensiuni și tipuri de hârtie, la finisaje și detalii unice de design. Procesul nostru simplu și eficient începe cu o consultare detaliată, urmată de un design creativ ce va depăși așteptările tale.</h2>
         </div>
+
+        <div className="forth_parallax">
+        <div className="ceata"></div>
+           <h3 className="branding_titles">DESIGN GRAFIC</h3>
+        </div>
+
+        
+        <div className="third_paravan">
+        <h1>Design grafic de top</h1>
+          <h2>Oferim soluții complete de design grafic, menite să te ajute să comunici eficient și să te diferențiezi pe piață. De la branding și identitate vizuală care reflectă personalitatea și valorile afacerii tale, până la materiale publicitare de impact precum broșuri, afișe și bannere, ne asigurăm că fiecare detaliu este aliniat perfect cu viziunea ta. Oferim și design digital pentru website-uri, aplicații mobile și social media, completat de animații și infografice care fac conținutul tău mai captivant. În plus, susținem campaniile tale de marketing prin publicitate creativă și conținut vizual optimizat pentru a atrage și angaja audiența. La Dersign Grafic, transformăm ideile tale în realitate!
+          </h2>
+        </div>
+
+        <Link to="/contact" className="branding_button">CERE OFERTA</Link>
 
     </div>
   )
