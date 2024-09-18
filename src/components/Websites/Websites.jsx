558378208938c0b599dcaf2eb9926924f4311e14
Rata Fabian <rata.fabian1@gmail.com>
2024-09-18 21:00:57 +0100

    responsive design for homepage

--- src/components/Websites/Websites.jsx
+++ src/components/Websites/Websites.jsx
@@ -46,11 +46,7 @@ useEffect(() => {
 
     randomMargins();
   }, []);
-
-
-// TO DO
-// addd examples for the users to see and 
-// maybe they gonna have more ideas on how to build the page        
+      
 
 
   return (
@@ -58,7 +54,7 @@ useEffect(() => {
       <div  className="websites_matrix_container">
       <h3 className="websites_titles">WEBSITES</h3>
          {matrixMargins.map((margins, index) => <span style={{marginLeft:`${margins}px`, animationDelay: `${index * 0.2}s`}} key={margins}>{matrix.map((number) => <p>{number}</p>)}</span>
-         )} 
+         )}
       </div>
 
 {/* another section */}          
@@ -68,9 +64,9 @@ useEffect(() => {
         <p>În era digitală, prezența online a afacerii tale este esențială pentru a atrage și reține clienți. Echipa noastră îți oferă soluții complete de dezvoltare web, de la design personalizat până la optimizare pentru performanță și suport continuu. Indiferent de nevoile tale, creăm website-uri care impresionează vizitatorii și generează rezultate reale.</p>
         </div>
 
-        <div className="websites_first_parallax">
+        <div className="websites_first_parallax website_computer_parallax">
            <div className="website_ceata"></div> 
-           <h3 className="websites_titles">WEBSITEURI PERSONALIZATE</h3>
+           <h3 className="websites_titles">WEBSITEURI UNICE</h3>
         </div>
 
         <div className="websites_green_text_contianer">
@@ -78,7 +74,7 @@ useEffect(() => {
           <p>Ne specializăm în designul de website-uri personalizate care reflectă identitatea brandului tău și se adaptează nevoilor tale specifice. Fie că ai nevoie de un site simplu și elegant sau de o platformă complexă, echipa noastră asigură un design unic, o experiență de utilizare captivantă și o funcționalitate fără cusur, care îți vor pune afacerea în evidență online.</p>
         </div>
 
-        <div className="websites_first_parallax" style={{backgroundImage: `url(${require("../../assets/images/websites/website_circuit.jpg")})`}}>
+        <div className="websites_first_parallax website_computer_parallax" style={{backgroundImage: `url(${require("../../assets/images/websites/website_circuit.jpg")})`}}>
         <div className="website_ceata"></div>
            <h3 className="websites_titles">Optimizare & Performanță</h3>
         </div>
@@ -88,7 +84,7 @@ useEffect(() => {
         <p>Website-urile noastre sunt construite având în vedere performanța și optimizarea pentru motoarele de căutare (SEO), asigurând timpi de încărcare rapizi și o clasare mai bună pe Google și alte motoare de căutare. Cu ajutorul nostru, website-ul tău nu doar va arăta bine, ci va și performa eficient, atrăgând mai mult trafic și generând rezultate mai bune pentru afacerea ta.</p>
       </div>
 
-        <div className="websites_forth_parallax" style={{backgroundImage: `url(${require("../../assets/images/websites/websites_servers.jpg")})`}}>
+        <div className="websites_forth_parallax website_computer_parallax" style={{backgroundImage: `url(${require("../../assets/images/websites/websites_servers.jpg")})`}}>
         <div className="website_ceata"></div>
            <h3 className="websites_titles">UPDATEURI</h3>
         </div>
