bcd3dd5fc5a0bc766338b19de899b985d2e17eb9
Rata Fabian <rata.fabian1@gmail.com>
2024-09-22 00:21:12 +0100

    Updates

---
+++ static/js/720.8de824e9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkartech=self.webpackChunkartech||[]).push([[720],{6720:(t,a,c)=>{c.d(a,{mm2:()=>e});var h=c(3441);function e(t){return(0,h.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"},child:[]},{tag:"path",attr:{d:"M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4-8 5-8-5V6l8 5 8-5v2z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=720.8de824e9.chunk.js.map
