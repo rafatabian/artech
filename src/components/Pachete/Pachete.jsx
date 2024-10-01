646c7056f00c0747ff99b0bd4f8c835a4a84c28d
Rata Fabian <rata.fabian1@gmail.com>
2024-10-01 21:41:10 +0100

    adding plans page translation

--- src/components/Pachete/Pachete.jsx
+++ src/components/Pachete/Pachete.jsx
@@ -3,13 +3,15 @@ import {  FaRocket, FaStar, FaCircleInfo  } from "react-icons/fa6"
 import { ImArrowUpRight2 } from "react-icons/im"
 import { FaCheckCircle } from "react-icons/fa"
 import { useEffect, useRef, useState } from "react"
-import { Link, useParams } from "react-router-dom"
+import { Link} from "react-router-dom"
 import { Helmet } from "react-helmet-async"
+import { useTranslation } from "react-i18next"
 
 
 const Pachete = () => {
   const [planFocus, setPlanFocus] = useState(2)
   const plansRef = useRef()
+  const [t] = useTranslation("global")
 
   // scroll to top and to plans details when redirected from anothe page
 useEffect(() => {
@@ -48,29 +50,29 @@ const handlePlanClick = (e) => {
       <div className="plans_presentation">
       <div className={`plans_first_plan ${planFocus === 0 ? "plans_focus_plan" : null}`} onClick={() => handlePlanClick(0)}>
          <span>STARTER <FaRocket /></span>
-         <span className="homePachetete_text"><FaCheckCircle />Design personalizat.</span>
-         <span className="homePachetete_text"><FaCheckCircle />Creare de logo simplu.</span>
-         <span className="homePachetete_text"><FaCheckCircle />Mini-site de prezentare.</span>
-         <span className="homePachetete_text"><FaCheckCircle />Setup pentru pagini pe social media.</span>
-         <button className="homePachete_alege_btn" onClick={() => setPlanFocus(0)}>ALEGE</button>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_starter1")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_starter2")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_starter3")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_starter4")}</span>
+         <button className="homePachete_alege_btn" onClick={() => setPlanFocus(0)}>{t("pachete_choose_btn")}</button>
         </div>
         <div className={`plans_third_plan ${planFocus === 2 ? "plans_focus_plan" : null}`} onClick={() => handlePlanClick(2)}>
          <span>STANDARD <ImArrowUpRight2/></span>
-         <span className="homePachetete_text"><FaCheckCircle />Design profesional</span>
-         <span className="homePachetete_text"><FaCheckCircle />Logo avansat</span>
-         <span className="homePachetete_text"><FaCheckCircle />Website de 5 pagini</span>
-         <span className="homePachetete_text"><FaCheckCircle />Creare de conținut și postări lunare.</span>
-         <span className="homePachetete_text"><FaCheckCircle />Campanie de publicitate</span>
-         <button className="homePachete_alege_btn" onClick={() => setPlanFocus(2)}>ALEGE</button>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_standard1")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_standard2")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_standard3")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_standard4")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_standard5")}</span>
+         <button className="homePachete_alege_btn" onClick={() => setPlanFocus(2)}>{t("pachete_choose_btn")}</button>
         </div>
         <div className={`plans_second_plan ${planFocus === 1 ? "plans_focus_plan" : null}`} onClick={() => handlePlanClick(1)}>
          <span>PRO <FaStar /></span>
-         <span className="homePachetete_text"><FaCheckCircle />Branding & Website complet</span>
-         <span className="homePachetete_text"><FaCheckCircle />Optimizare SEO si performanta</span>
-         <span className="homePachetete_text"><FaCheckCircle />Gestionare completă a campaniilor de publicitate</span>
-         <span className="homePachetete_text"><FaCheckCircle />Creare de conținut săptămânal</span>
-         <span className="homePachetete_text"><FaCheckCircle />Consultanță și updateuri lunare</span>
-         <button className="homePachete_alege_btn" onClick={() => setPlanFocus(1)}>ALEGE</button>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_pro1")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_pro2")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_pro3")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_pro4")}</span>
+         <span className="homePachetete_text"><FaCheckCircle />{t("pachete_pro5")}</span>
+         <button className="homePachete_alege_btn" onClick={() => setPlanFocus(1)}>{t("pachete_choose_btn")}</button>
         </div>
         </div>
         {/* constent under plans */}  
@@ -78,50 +80,48 @@ const handlePlanClick = (e) => {
           {planFocus === 0 ?
          <div className="plans_content_starter">
           <h1>STARTER</h1>
-          <p>Pentru cei care doresc sa isi contureze identitatea de brand, acest pachet ofera fundatia perfecta</p>
-          <div><span><FaCheckCircle />Creearea unui logo simplu:</span> <p>Un design elegant care capteaza esenta afacerii tale. Fiecare element este ales cu grija pentru a reflecta voalorile si personalitatea brandului tau.</p></div>
-          <div><span><FaCheckCircle />Minisite de prezentare:</span> <p>O pagina web moderna, optimizata pentru a oferi informatii esentiale despre serviciile tale, intru-un formatr atragator si accesibil.</p></div>
-          <div><span><FaCheckCircle />Design personalizat pentru social media:</span> <p>Creearea unui set de imagini si grafica unica, adaptate pentru fiecare platforma, care sa iti sporeasca vizibilitatea si atractivitatea online.</p></div>
+          <p>{t("pachete_page_starter_sub")}</p>
+          <div><span><FaCheckCircle />{t("pachete_page_starter_title1")}</span> <p>{t("pachete_page_starter_p1")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_starter_title2")}</span> <p>{t("pachete_page_starter_p2")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_starter_title3")}</span> <p>{t("pachete_page_starter_p3")}</p></div>
          </div> 
          : null}
 
          {planFocus === 2 ? 
          <div className="plans_content_standard" >
           <h1>STANDARD</h1>
-          <p>Ideal pentru afaceri in expansiune, acest pachet combina creativitatea cu strategia pentru a-ti transforma prezenta online</p>
-          <div><span><FaCheckCircle />Design personalizat:</span><p>Un branding exclusivist, creat spacial pentru tine, care sa rezoneze cu publicul tinta si sa se distanteze de consurenta.</p></div>
-          <div><span><FaCheckCircle />Logo avansat:</span><p>Un logo complex si memorabil, realizat cu tehnici de design de varf, pentru a transmite profesionalism si incredere.</p></div>
-          <div><span><FaCheckCircle />Website de pana la 5 pagini:</span><p>O platforma eleganta, care ofera o experienta de navigare fluida si placuta pentru utilizatori</p></div>
-          <div><span><FaCheckCircle />Creare de continut pntru postarile lunare:</span><p>Strategii de continut bine gandite care sa atraga si sa retina atentia audientei tale.</p></div>
-          <div><span><FaCheckCircle />Campanie de publicitate:</span><p>Solutii strategica de marketing, adaptate spacific pentru a creste vizibilitatea brandului tau si a atrage clienti noi.</p></div>
+          <p>{t("pachete_page_standard_sub")}</p>
+          <div><span><FaCheckCircle />{t("pachete_page_standard_title1")}</span><p>{t("pachete_page_standard_p1")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_standard_title2")}</span><p>{t("pachete_page_standard_p2")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_standard_title3")}</span><p>{t("pachete_page_standard_p3")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_standard_title4")}</span><p>{t("pachete_page_standard_p4")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_standard_title5")}</span><p>{t("pachete_page_standard_p5")}</p></div>
          </div>
          : null}
 
          {planFocus === 1 ? 
          <div className="plans_content_pro">
           <h1>PRO</h1>
-          <p>Pe langa serviciile din pachetele anterioare, aveti si:</p>
-          <div><span><FaCheckCircle />Optinizare SEO si performanta:</span><p>Strategii de optimizare care asigura ca site-ul tau este nu doar vizibil, ci si preferat de motoarele de cautare.</p></div>
-          <div><span><FaCheckCircle />Consultanta continua:</span><p>O echipa de experti care iti va oferi indrumari si sugestii personalizate, adaptate evolutiei afacerii tale.</p></div>
-          <div><span><FaCheckCircle />Update-uri lunare</span><p>intretinerea constanta a siteului si actualizarea continutului pentru a menitine un standard inalt si o relevanta constanta</p></div>
-          <div><span><FaCheckCircle />Domeniu gratuit</span><p>De exemplu  'afacerea-mea.com'. Ne ocupam noi sa platim domeniul siteului tau timp de un an.</p></div>
-          <div><span><FaCheckCircle />Server Hosting</span><p>Pentru a-ți asigura un site sigur și vizibil în căutările Google, îl găzduim pe un server performant, oferindu-ți gratuit un an de hosting inclus din partea noastră.</p></div>
+          <p>{t("pachete_page_pro_sub")}</p>
+          <div><span><FaCheckCircle />{t("pachete_page_pro_title1")}</span><p>{t("pachete_page_pro_p1")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_pro_title1")}</span><p>{t("pachete_page_pro_p2")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_pro_title1")}</span><p>{t("pachete_page_pro_p3")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_pro_title1")}</span><p>{t("pachete_page_pro_p4")}</p></div>
+          <div><span><FaCheckCircle />{t("pachete_page_pro_title1")}</span><p>{t("pachete_page_pro_p5")}</p></div>
 
          </div>
          : null}
 
          <div className="plans_button_container">
-            <Link to={`/contact/#/${planFocus === 0 ? "starter" : planFocus === 1 ? "pro" : "standard"}`}>ALEGE PLANUL {`${planFocus === 0 ? "STARTER" : planFocus === 1 ? "PRO" : "STANDARD"}`}</Link>
+            <Link to={`/contact/#/${planFocus === 0 ? "starter" : planFocus === 1 ? "pro" : "standard"}`}> {t("pachete_page_button")} {`${planFocus === 0 ? "STARTER" : planFocus === 1 ? "PRO" : "STANDARD"}`}</Link>
          </div>
         </div>
 
         <div className="plans_final_text">
-          <span>Credem ca fiecare proiect este unic si merita o abordare personalizata. De aceea, preturile noastre sunt adaptate specific nevoilor si cerintelor fiecarui client.
-            Costul final depinde de complexitatea proiectului, de timpul necesar pentru realizare si de volumul de munca implicat. Te incurajam sa ne cotactezi pentru a discuta despre viziunea ta, astfle incat sa putem oferi o oferta personalizata, care sa reflecte exact ceea ce iti doresti. 
-            Asteptam cu interes sa colaboram si sa transormam ideile tale in realitate.
+          <span>We believe that every project is unique and deserves a tailored approach. That's why our pricing is customized to meet the specific needs and requirements of each client. The final cost depends on the complexity of the project, the time needed for completion, and the scope of work involved. We encourage you to contact us to discuss your vision, so we can provide a personalized quote that reflects exactly what you're looking for. We look forward to working together and bringing your ideas to life.
           </span>
           <div className="plans_button_container">
-          <Link to="/contact">CERE O OFERTA</Link>  
+          <Link to="/contact">{t("branding_contact")}</Link>  
           </div>
             
        </div>
