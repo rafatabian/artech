e637b78bb761dda5d5e3c1ac76e9f84cc0c8d938
Fabian Rata <fabianrata@Fabians-Mac-mini.local>
2025-06-09 18:49:55 +0100

    changing home btn name and testing for safari video issue

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,89 +1,107 @@
-import "./Home.css"
-import "../../index.css"
-import homeVideo from "../../assets/videos/artech_home_video.mp4"
+import "./Home.css";
+import "../../index.css";
+import homeVideo from "../../assets/videos/artech_home_video.mp4";
 import { Link } from "react-router-dom";
 import { lazy, useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import { useTranslation } from "react-i18next";
-const NewHomeWebsite = lazy(() => import("../../components/HomeComponents/HomeWebsite/HomeWebsite.jsx"))
-const HomeMarketing = lazy(()=> import("../../components/HomeComponents/HomeMarketing/HomeMarketing.jsx"))
-const HomeBranding = lazy(() => import("../../components/HomeComponents/HomeLogos/HomeLogos.jsx"))
-const HomePachete = lazy(() => import("../../components/HomeComponents/HomePachete/HomePachete.jsx"))
-const HomeBlogs = lazy(() => import("../../components/HomeComponents/HomeBlogs/HomeBlogs.jsx"))
-const HomeServices = lazy(() => import("../../components/HomeComponents/Home_services/Home_services.jsx"))
-const HomeOffer = lazy(() => import("../../components/HomeComponents/Home_offer/Home_offer.jsx"))
-
-
-
+const NewHomeWebsite = lazy(() =>
+  import("../../components/HomeComponents/HomeWebsite/HomeWebsite.jsx")
+);
+const HomeMarketing = lazy(() =>
+  import("../../components/HomeComponents/HomeMarketing/HomeMarketing.jsx")
+);
+const HomeBranding = lazy(() =>
+  import("../../components/HomeComponents/HomeLogos/HomeLogos.jsx")
+);
+const HomePachete = lazy(() =>
+  import("../../components/HomeComponents/HomePachete/HomePachete.jsx")
+);
+const HomeBlogs = lazy(() =>
+  import("../../components/HomeComponents/HomeBlogs/HomeBlogs.jsx")
+);
+const HomeServices = lazy(() =>
+  import("../../components/HomeComponents/Home_services/Home_services.jsx")
+);
+const HomeOffer = lazy(() =>
+  import("../../components/HomeComponents/Home_offer/Home_offer.jsx")
+);
 
 const Home = () => {
-  const [t] = useTranslation("global")
+  const [t] = useTranslation("global");
 
-// scroll to top
-useEffect(() => {
-    window.scrollTo(0, 0)
-}, [])
+  // scroll to top
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-
-  return ( 
+  return (
     <>
-    <Helmet>
-      <title>Home</title>
-      <meta name="description" content="Aduti afacerea la urmatorul nivel cu Artech, fie ca vine vorba de pagini web, marketing, branding sau logouri, noi suntem cheia. "/>
-      <link rel="canonical" href="https://artech-agency.co/"/>
-    </Helmet>
-    <div className="home_container">
-      {/* new home section */}
-      <div className="home_new_container"> 
-        <div className="home_new_presentation">
+      <Helmet>
+        <title>Home</title>
+        <meta
+          name="description"
+          content="Aduti afacerea la urmatorul nivel cu Artech, fie ca vine vorba de pagini web, marketing, branding sau logouri, noi suntem cheia. "
+        />
+        <link rel="canonical" href="https://artech-agency.co/" />
+      </Helmet>
+      <div className="home_container">
+        {/* new home section */}
+        <div className="home_new_container">
+          <div className="home_new_presentation">
             <div className="home_new_title">
               <h1>ARTECH</h1>
               <h1>REDEFINED</h1>
-           </div>
-          
-          
-          <p>{t("home_intro")}</p>
-          <div className="home_new_btns_container">
-            {/* add link to section */}
+            </div>
+
+            <p>{t("home_intro")}</p>
+            <div className="home_new_btns_container">
+              {/* add link to section */}
               <Link to="/plans">{t("home_intro_first_btn")}</Link>
-              <Link to="/contact" >{t("home_get_a_quote")}</Link>
+              <Link to="/contact">{t("home_consultation")}</Link>
+            </div>
+          </div>
+
+          <div className="home_video">
+            <video
+              src={homeVideo}
+              autoPlay
+              loop
+              muted
+              playsInline
+              controlsList="nofullscreen"
+              preload="auto"
+            />
           </div>
         </div>
-        
-        <div className="home_video">
-          <video src={homeVideo} autoPlay loop muted playsInline  controlsList="nofullscreen" preload="auto"/>
+        {/* our services */}
+        <HomeServices />
+
+        {/*  */}
+        {/* 100pounds offer */}
+        <HomeOffer />
+
+        {/* logos */}
+        <HomeBranding />
+
+        {/* websites */}
+        <div className="home_websites">
+          {/* <HomeWebsites /> */}
+          <NewHomeWebsite />
         </div>
-         
+        {/* marketing */}
+        <div className="home_branding">
+          <HomeMarketing />
+        </div>
+        {/* pachete  */}
+        <div className="home_pachete">
+          <HomePachete />
+        </div>
+
+        <HomeBlogs />
       </div>
-{/* our services */}
-      <HomeServices />
-      
-      {/*  */}
-{/* 100pounds offer */}
-    <HomeOffer />
-      
-{/* logos */}
-      <HomeBranding />
+    </>
+  );
+};
 
-{/* websites */}
-      <div className="home_websites">
-{/* <HomeWebsites /> */}
-         <NewHomeWebsite />
-      </div>
-{/* marketing */}
-      <div className="home_branding">
-        <HomeMarketing />
-      </div>
-{/* pachete  */}
-      <div className="home_pachete">
-        <HomePachete />
-      </div>
-
-      <HomeBlogs />
- </div>
- </>
-  )}
-
-
-
-export default Home
+export default Home;
