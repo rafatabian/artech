69ee7dd19d17be05b21683da605518404099dad0
Fabian Rata <fabianrata@Fabians-Mac-mini.local>
2025-06-15 11:59:17 +0100

    testing for homeservices error

--- src/components/HomeComponents/Home_services/home_services.jsx
+++ src/components/HomeComponents/Home_services/home_services.jsx
@@ -1,13 +1,13 @@
 import "./Home_services.css"
-import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
+// import { Link } from "react-router-dom";
+// import { useTranslation } from "react-i18next";
 
 const Home_services = () => {
-  const [t] = useTranslation("global");
+  // const [t] = useTranslation("global");
 
   return (
     <div className="home_our_services">
-   <h2>{t("home_our_services_title")}:</h2>
+   {/* <h2>{t("home_our_services_title")}:</h2>
 <ul>
   <li> <Link to="/websites">- {t("home_our_services_1")}</Link> </li>
   <li> <Link to="/websites">- {t("home_our_services_2")}</Link> </li>
@@ -20,7 +20,7 @@ const Home_services = () => {
   <li> <Link to="/marketing">- {t("home_our_services_9")}</Link> </li>
   <li> <Link to="/marketing">- {t("home_our_services_10")}</Link> </li>
   <li> <Link to="/marketing">- {t("home_our_services_11")}</Link> </li>
-</ul>
+</ul> */}
 
   </div>
   )
